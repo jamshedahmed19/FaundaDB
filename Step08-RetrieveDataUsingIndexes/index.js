55842585d4e8d65a88389310e9b5e5ef5b7b69cc
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-02-17 20:45:37 +0500

    FaunaDB step 08 to retrieve data using indexes

---
+++ Step08-RetrieveDataUsingIndexes/index.js
@@ -0,0 +1,24 @@
+const faunadb = require("faunadb");
+require("dotenv").config();
+
+const q = faunadb.query;
+
+(async () => {
+  if (process.env.FAUNDADB_SERVER_SECRET) {
+    var client = new faunadb.Client({
+      secret: process.env.FAUNDADB_SERVER_SECRET,
+    });
+
+    try {
+      var result = await client.query(
+        q.Get(q.Match(q.Index("all_blogs"), "Test Blog 01"))
+      );
+
+      console.log("result: ", result);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  } else {
+    console.log("Server Key Doesn't Exist");
+  }
+})();
